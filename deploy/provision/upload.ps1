7254f77c70e0261792a7a64ad9673338f29fffc9
Oliver Damsgaard Jensen <oliver.damsgaard.jensen@if.no>
2021-03-25 13:22:45 +0100

    fix param names

--- deploy/provision/upload.ps1
+++ deploy/provision/upload.ps1
@@ -1,18 +1,18 @@
 Param(
-    [parameter(mandatory=$true)][string]$appname,
-    [parameter(mandatory=$true)][string]$pathtozip,
-    [parameter(mandatory=$true)][string]$resourcegroup,
-    [parameter(mandatory=$true)][string]$subscription
+    [parameter(mandatory=$true)][string]$Application,
+    [parameter(mandatory=$true)][string]$PathToZip,
+    [parameter(mandatory=$true)][string]$ResourceGroup,
+    [parameter(mandatory=$true)][string]$Subscription
 )
 
     
 Write-Host "Deploying $zip"
-$deploy = az webapp deployment source config-zip -n $appname --src $pathtozip -g $resourcegroup --subscription $subscription --query "status"
+$deploy = az webapp deployment source config-zip -n $Application --src $PathToZip -g $ResourceGroup --subscription $Subscription --query "status"
 # status 4 = success - status 3 = failed
 if ([string]$deploy -ne '4') 
 { 
-    Write-Error "Deployment Failed, see https://$appname.scm.azurewebsites.net/zipdeploy for details"; 
+    Write-Error "Deployment Failed, see https://$Application.scm.azurewebsites.net/zipdeploy for details"; 
     EXIT 1 
 } 
 
-Write-Host "Successfully deployed $appname"
+Write-Host "Successfully deployed $Application"
